731d1fad8362eb78545de518bfae1ccfe47e3382
Kyohei Oka <0622okakyo@gmail.com>
2018-12-06 12:09:30 +0900

    '練習中'

---
+++ basic/sample.java
@@ -0,0 +1,6 @@
+class sample{
+	public static void main(String[] arg){
+		for(int i=0;i<10;i++)
+			System.out.println("Hello");
+	}
+}
---
+++ basic/sample2.java
@@ -0,0 +1,11 @@
+//(ファイル名).javaで、class (ファイル名)内の関数は、
+//public static void main(String[] arg){}でしか記述できない。
+
+class sample2{
+	public static void main(String[] arg){
+		double pi=3.1415;
+		int R=3;
+		double ans=pi*R*R;
+		System.out.println(ans);	
+	}
+}
