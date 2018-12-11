12898fe8a5dc8e4c3b8c6273afa6cad1f1e6463c
Kyohei Oka <0622okakyo@gmail.com>
2018-12-11 14:37:26 +0900

    '練習中'

---
+++ Game/window.java
@@ -0,0 +1,50 @@
+import java.awt.Graphics;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+class window{
+	
+	public static void main(String[] args){
+		GameWindow gw=new GameWindow("ƒeƒXƒg",640,480);
+		gw.add(new DrawCanvas());
+		gw.setVisible(true);
+
+	}
+
+}
+
+
+
+class GameWindow extends JFrame{
+	public GameWindow(String title,int width,int height){
+	super(title);
+	setDefaultCloseOperation(EXIT_ON_CLOSE);
+	setSize(width,height);
+	setLocationRelativeTo(null);
+	setResizable(false);
+	}
+
+}
+
+class DrawCanvas extends JPanel{
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		g.drawLine(100,100,200,200);
+	}
+}
+
+class control{
+	public 
+}
+class Robots{
+	double R,w,h,sp;
+	double P,I,D;
+ 	public:
+		void Robots(){}
+		void Control(){};
+
+}
