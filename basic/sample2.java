ffb76a705df90b6460fd29e0321083f6553e6c20
Kyohei Oka <0622okakyo@gmail.com>
2019-03-27 11:11:10 +0900

    '修正中'

--- basic/sample2.java
+++ basic/sample2.java
@@ -1,11 +1,5 @@
-//(ファイル名).javaで、class (ファイル名)内の関数は、
-//public static void main(String[] arg){}でしか記述できない。
-
 class sample2{
-	public static void main(String[] arg){
-		double pi=3.1415;
-		int R=3;
-		double ans=pi*R*R;
-		System.out.println(ans);	
+	public static void main(String arg){
+		System.out.println("Hello World!");
 	}
 }
